ffdc8e579a22dc29d718d1ef697f9ecd8382be73
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-02-08 14:22:39 +0300

    Мой код с урока

---
+++ js/bouquet.js
@@ -0,0 +1,45 @@
+// Дано:
+// розы (маленькие - 4 расцветки, большие - 7 расцветок)
+// трава (5 видов)
+// лилии - 3 цвета
+// тюльпаны - 3 цвета
+// пионы - 2 цвета
+// герберы - 5 цветов
+
+// Функция-конструктор цветка
+function Flower(name, size, color)
+{
+    // properties
+    this.name = name;
+    this.size = size;
+    this.color = color;
+    
+    // getters and setters
+
+    // methods
+    this.info = function info()
+    {
+        alert("Мои данные: " + name + size + color);
+    };
+}
+
+
+let rose = new Flower("rose", "small", "red");
+rose.info();
+
+let rosesAmmount = 4;
+
+let roses = [];
+for (let flower = 0; flower < rosesAmmount; flower++)
+{
+    roses[flower] = new Flower("rose", "small", "red");
+}
+
+let grassAmmount = 0;
+let grass = [];
+for (let grass = 0; grass < array.length; grass++)
+{
+    const element = array[grass];
+}
+
+
