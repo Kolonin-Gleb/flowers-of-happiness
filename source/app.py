7500baa2ba3879eed42203ad01fef8ebd730beac
Kolonin-Gleb <56352942+Kolonin-Gleb@users.noreply.github.com>
2022-03-28 16:46:58 +0300

    Начало продумывания маршрутов к БД
    Начало продумывания маршрутов к БД.
    Продумывание функционала сайта.

--- source/app.py
+++ source/app.py
@@ -1,3 +1,7 @@
+# app.py является неким API.
+# API позволяет позволяет описать действия по обработке поступающих запросов.
+# Сами же запросы поступают с помощью Ajax запросов.
+
 # Для разработки сайта 
 from flask import Flask
 
@@ -15,13 +19,16 @@ from datetime import datetime
 # Папка static будет содержать неизменяемый JS и CSS
 app = Flask(__name__, static_folder="static")
 
-# Для конвертации времени из SQL в строку
-# Настройки для соединения с БД.
-conv = pymysql.converters.conversions.copy()
-conv[12] = str # Конвертация формата времени получаемого из БД в строку, формат которой позволяет загрузить данные обратно в БД 
 
-print("Просмотр кодов для настроек: \n")
-print(conv)
+# Настройка соединения с БД.
+# Она позволяет конвертировать время из SQL в строку
+conv = pymysql.converters.conversions.copy()
+conv[12] = str # Конвертация формата времени получаемого из БД в строку,
+# формат которой позволяет загрузить данные обратно в БД.
+# Это необходимо для сохранения времени оформления заказа
+
+# print("Просмотр кодов для настроек: \n")
+# print(conv)
 
 # Установка соединения с БД
 dbh = pymysql.connect(
@@ -44,40 +51,17 @@ def main_page():
     html.close()
     return page
 
-@app.route('/authors')
-def authors_page():
-    html = open("authors.html", "r")
-    page = html.read()
-    html.close()
-    return page
-
-@app.route('/categories')
-def categories_page():
-    html = open("categories.html", "r")
-    page = html.read()
-    html.close()
-    return page
-
-@app.route('/articles')
-def articles_page():
-    html = open("articles.html", "r")
-    page = html.read()
-    html.close()
-    return page
-
-# Обработка запросов к странице authors #################################################
-
-@app.route('/get_author_list')
-def get_author_list():
+# Запрос на получение ассортимента
+@app.route('/get_products_list')
+def get_products_list():
     try:
         with dbh.cursor() as cur:
-            cur.execute('SELECT * FROM blog_authors')
-            authors = cur.fetchall()
+            cur.execute('SELECT * FROM ???') # Получить все данные для отображения ассортимента
+            products = cur.fetchall()
     except:
-        authors = { 'error': 'Ошибка чтения таблицы авторов' }
+        products = { 'error': 'Ошибка чтения таблицы продукции' }
 
-    return jsonify(authors)
-
+    return jsonify(products)
 
 @app.route('/get_author', methods=['POST'])
 def get_author():
@@ -168,262 +152,5 @@ def save_author():
 
     return jsonify(out_data)
 
-# Обработка запросов к странице categories #################################################
-
-@app.route('/get_category_list')
-def get_category_list():
-    try:
-        with dbh.cursor() as cur:
-            cur.execute('SELECT * FROM blog_categories')
-            categories = cur.fetchall()
-    except:
-        categories = { 'error': 'Ошибка чтения таблицы авторов' }
-
-    return jsonify(categories)
-
-
-@app.route('/get_category', methods=['POST'])
-def get_category():
-    out_data = {'status': 'error'}
-    id = request.form.get('id') # получение id строчки категории из таблицы
-
-    # Действия над существующей категорией
-    if int(id) > 0:
-        try:
-            with dbh.cursor() as cur:
-                # Получение данных из таблицы о категории
-                cur.execute('SELECT * FROM blog_categories WHERE id='+str(id))
-                category_data = cur.fetchall()
-                out_data = {
-                    'status': 'ok',
-                    'category': category_data[0], 
-                }
-        except:
-            out_data = {
-                'status': 'error'
-            }
-    # Создание новой категории
-    else:
-        new_category = {
-            'category': '',
-            'id': 0
-        }
-        out_data = {
-            'status': 'ok',
-            'category': new_category,
-        }
-    return jsonify(out_data)
-
-
-@app.route('/delete_category', methods=['POST'])
-def delete_category():
-    out_data = {'status': 'error'}
-    id = request.form.get('id')
-
-    if int(id) > 0:
-        try:
-            with dbh.cursor() as cur:
-                cur.execute('DELETE FROM blog_categories WHERE id = '+str(id))
-                out_data = {
-                    'status': 'ok',
-                }
-        except:
-            out_data = {
-                'status': 'error'
-            }
-
-    return jsonify(out_data)
-
-
-# Эта функция сохраняет данные формы
-@app.route('/save_category', methods=['POST'])
-def save_category():
-    # Получение данных data с помощью библ. request
-    id = int(request.form.get('id'))
-    category = request.form.get('category')
-
-    # Проверка получения данных
-    print(f"{id}, {category}")
-
-    sql = ''
-    if id > 0:
-        sql = f"UPDATE blog_categories SET category='{category}' WHERE id={id}"
-    else:
-        sql = "INSERT INTO blog_categories (category)"
-        sql += f" VALUE ('{category}')"
-
-    # Попытка выполнить sql
-    print(sql)
-    try:
-        with dbh.cursor() as cur:
-            cur.execute(sql)
-            out_data = {
-                'status': 'ok'
-            }
-    except:
-        out_data = {
-            'status': 'error'
-        }
-
-    return jsonify(out_data)
-
-# Обработка запросов к странице articles #################################################
-
-@app.route('/get_article_list')
-def get_article_list():
-    try:
-        with dbh.cursor() as cur:
-            # Получение всех данных из представления
-            cur.execute('SELECT * FROM blog_articles_full')
-            articles = cur.fetchall()
-    except:
-        articles = { 'error': 'Ошибка чтения статей' }
-
-    return jsonify(articles)
-
-
-@app.route('/get_article', methods=['POST'])
-def get_article():
-    out_data = {'status': 'error'}
-    id = request.form.get('id') # получение id строчки категории из таблицы
-
-    categories = ''
-    # Взятие всех категорий из внешней таблицы
-    try:
-        with dbh.cursor() as cur:
-            cur.execute('SELECT * FROM blog_categories')
-            categories = cur.fetchall()
-    except:
-        out_data = {
-            'status': 'error',
-            'text': 'При загрузке всех категорий из внешней таблицы'
-        }
-        return jsonify(out_data)
-
-    authors = ''
-    # Взятие всех авторов из внешней таблицы
-    try:
-        with dbh.cursor() as cur:
-            cur.execute('SELECT id, CONCAT_WS(" ", a.f, a.i, a.o) AS fio FROM blog_authors a')
-            authors = cur.fetchall()
-    except pymysql.Error as e: # Получение кода ошибки pymysql
-        print("could not close connection error pymysql %d: %s" %(e.args[0], e.args[1]))
-        out_data = {
-            'status': 'error'
-        }
-        return jsonify(out_data)
-
-    # Действия над существующей категорией
-    if int(id) > 0:
-        try:
-            with dbh.cursor() as cur:
-                # Получение данных о конкретной статье
-                cur.execute('SELECT * FROM blog_articles_full WHERE id='+str(id))
-                article_data = cur.fetchall()
-
-                print("Полученные данные о конретной статье в виде словаря:")
-                print(article_data[0])
-
-                out_data = {
-                    'status': 'ok',
-                    # Передача данных о выбранной статье
-                    'article': article_data[0],
-                    # Передача данных о возможных категориях и авторах. Нужно для редактирования статьи
-                    'categories': categories, # Возможные категории из внешней таблицы
-                    'authors': authors, # Возможные авторы из внешней таблицы
-                }
-        except:
-            out_data = {
-                'status': 'error'
-            }
-    # Создание новой статьи
-    else:
-        print("Начало создание статьи с id" + str(id))
-        now = datetime.now() # Получение тек. времени
-        now = now.strftime('%Y-%m-%d %H:%M:%S') # Конвертация времени в формат DATETIME MySQL
-        # В итоге now хранит время в формате 2021-12-26 08:41:28
-
-        new_article = {
-            'id': 0,
-            'fio': '',
-            'category': '',
-            'title': '',
-            'article': '',
-            'dt': now,
-        }
-        out_data = {
-            'status': 'ok',
-            'article': new_article, # Передача данных о новой статье с значениями по умолчанию
-            # Передача данных о возможных категориях и авторах. Необходимо для выбора при редактировании
-            'categories': categories,
-            'authors': authors
-        }
-    return jsonify(out_data)
-
-
-@app.route('/delete_article', methods=['POST'])
-def delete_article():
-    out_data = {'status': 'error'}
-    id = request.form.get('id')
-
-    if int(id) > 0:
-        try:
-            with dbh.cursor() as cur:
-                cur.execute('DELETE FROM blog_articles WHERE id = '+str(id))
-                out_data = {
-                    'status': 'ok',
-                }
-        except:
-            out_data = {
-                'status': 'error'
-            }
-
-    return jsonify(out_data)
-
-
-@app.route('/save_article', methods=['POST'])
-def save_article():
-    # Получение данных data с помощью библ. request
-    id = int(request.form.get('id'))
-    fio = str(request.form.get('fio')) #TODO: переименовать в fio_id или author_id
-    category = str(request.form.get('category')) #TODO: переименовать в category_id
-    title = str(request.form.get('title'))
-    article = str(request.form.get('article'))
-    dt = str(request.form.get('dt'))
-
-    print("Данные полученные с фронтенда для сохранения в БД: \n")
-    print(f"{id}, {fio}, {category}, {title}, {article}, {dt}")
-
-    # Обновление таблицы статей
-    sql = ''
-    if id > 0:
-        sql  = f"UPDATE blog_articles SET category_id='{category}', author_id={fio}, "
-        sql += f"title='{title}', article='{article}', dt='{dt}' "
-        sql += f"WHERE id={id}"
-    else:
-        sql = "INSERT INTO blog_articles (category_id, author_id, title, article, dt)"
-        sql += f" VALUE ('{fio}', '{category}', '{title}', '{article}', '{dt}')"
-
-    # Попытка выполнить sql
-    print("SQL запрос для выполнения: \n")
-    print(sql)
-
-    try:
-        with dbh.cursor() as cur:
-            cur.execute(sql)
-            out_data = {
-                'status': 'ok'
-            }
-    except:
-        out_data = {
-            'status': 'error'
-        }
-
-    return jsonify(out_data)
-
-
 # Запуск приложения на сервере колледжа
 app.run(debug = True, host='db-learning.ithub.ru', port=1177)
-
-# Запуск приложения на локальном пк
-# app.run(debug=True)
---
+++ source/static/js/products.js
@@ -0,0 +1,183 @@
+// Здесь должен находиться код, который позволит
+// отправлять запросы на сервер и получать ответы.
+// Для этого используется технология Ajax запросов.
+// JavaScript будет барть данные из результатов Ajax запросов 
+// и отображать их на фронтенде
+
+$(function() {
+	$('#author_list').text('Идёт загрузка списка авторов');
+	loadAuthorList();
+});
+
+$('#save_author').click(function () {
+    console.log('Сохранение автора');
+    saveAuthor();
+});
+
+function renderEditButton() {
+    $('.edit_button').click(function (e) {
+        console.log('Редактирование автора');
+        var el = e.target;
+        var id = el.getAttribute('data-id');
+        getAuthor(id);
+        console.log(id);
+    });
+
+    $('.delete_button').click(function (e) {
+        console.log('Удаление автора');
+        var el = e.target;
+        var id = el.getAttribute('data-id');
+        deleteAuthor(id);
+        console.log(id);
+    });
+
+    $('#add_button').click(function () {
+        console.log('Добваление автора');
+        getAuthor(0);
+        console.log("Объект: "+id);
+    });
+}
+
+function loadAuthorList()
+{
+    $.ajax({
+        url: '/get_author_list',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+            if (data instanceof Array)
+            {
+				console.log("Успешная загрузка авторов");
+                renderAuthorList(data);
+            }
+            else
+            {
+				console.log("Ошибка при загрузке авторов");
+            }
+        },
+        error: function(jqxhr, status, errorMsg) {
+            console.log('Ошибка при взаимодействии с сервером: ' + errorMsg)
+        }
+    });
+}
+
+// Загрузка конкретного контакта
+function getAuthor(id)
+{
+    $.ajax({
+        url: '/get_author',
+        type: 'POST',
+        dataType: 'json',
+        // Запрос на данные об 1 авторе по id
+        data: {
+            id: id
+        },
+        success: function (data) {
+            console.log(data);
+            if (data.status == 'ok')
+            {
+                console.log("Автор загружен");
+                renderForm(data.author);
+            }
+            else
+            {
+                console.log("Ошибка при загрузке автора");
+            }
+        },
+        error: function(jqxhr, status, errorMsg) {
+            console.log('Ошибка при взаимодействии с сервером: ' + errorMsg);
+        }
+    });
+}
+
+function deleteAuthor(id)
+{
+    $.ajax({
+        url: '/delete_author',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            id: id
+        },
+        success: function (data) {
+            console.log(data);
+            if (data.status == 'ok')
+            {
+                console.log("Автор удален");
+                loadAuthorList();
+            }
+            else
+            {
+                console.log("Ошибка при удалении автора");
+            }
+        },
+        error: function(jqxhr, status, errorMsg) {
+            console.log('Ошибка при взаимодействии с сервером: ' + errorMsg);
+        }
+    });
+}
+
+// Передача данных из формы на backend
+function saveAuthor()
+{
+    $.ajax({
+        url: '/save_author',
+        type: 'POST',
+        dataType: 'json',
+        // Взятие данных из формы по атрибуту id, упаковка их в data и отправка на url: '/save_author'
+        data: {
+            id: $('#id').val(),
+            f: $('#f').val(),
+            i: $('#i').val(),
+            o: $('#o').val(),
+        },
+        // Обработка ответа на запрос return jsonify(out_data)
+        success: function (data) {
+            console.log(data);
+            if (data.status == 'ok')
+            {
+                console.log("Автор сохранен");
+                loadAuthorList();
+                $('#author_form').hide();
+            }
+            else
+            {
+                console.log("Ошибка при сохранении автора");
+            }
+        },
+        error: function(jqxhr, status, errorMsg) {
+            console.log('Ошибка при взаимодействии с сервером: '+errorMsg);
+        }
+    });
+}
+
+// Отображение списка авторов
+function renderAuthorList(data)
+{
+    var html = '<p><button class="btn btn-primary" id="add_button">Добавить автора</button></p>';
+    html += '<table class="table table-bordered small">';
+    // Оформляем каждый элемент в виде строчки у таблицы
+    data.forEach(function(item, i, data) {
+        html += "<tr>";
+        html += "<td>"+item['f']+"</td>";
+        html += "<td>"+item['i']+"</td>";
+        html += "<td>"+item['o']+"</td>";
+        html += '<td><i class="bi bi-pencil-square edit_button" data-id="'+item['id']+'"></i></td>';
+        html += '<td><i class="bi bi-trash delete_button" data-id="'+item['id']+'"></i></td>';
+        html += "</tr>";
+    });
+    html += "</table>";
+
+    $('#author_list').html(html);
+    renderEditButton();
+}
+
+function renderForm(data) {
+    $('#id').val(data['id']),
+    $('#f').val(data['f']),
+    $('#i').val(data['i']),
+    $('#o').val(data['o']),
+
+    $('#author_form').show();
+}
